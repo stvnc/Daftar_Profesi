c682f3795cfccf12a28c883cf8b1999b3cc00877
stvnc <vincentangelo96@yahoo.com>
2019-12-19 12:25:07 +0700

    updated repo

--- daftar_profesi.py
+++ daftar_profesi.py
@@ -7,8 +7,13 @@ profesi= df['occupation'].unique()
 jumlahProfesi= df['occupation'].nunique()
 print (f'jumlah profesi : {jumlahProfesi}')
 
-df = df.set_index(["occupation","gender"])
-
-newDf = pd.DataFrame(profesi, index = ['occupation'])
-
+newDf = df.groupby([df['occupation'],df['gender']]).describe()
+newDf = newDf['age'][['max','min','mean']]
+newDf.rename(columns={'max':'max_usia','min':'min_usia','mean':'rerata_usia'}, inplace=True)
 print(newDf)
+
+gender = pd.crosstab(df.occupation, df.gender).apply(lambda r: r/r.sum(), axis=1) * 100
+gender['%total'] = gender['F']+gender['M']
+gender.rename(columns={'F':'%female','M':'%male'},inplace=True)
+gender = gender[['%male','%female','%total']]
+print(gender)
