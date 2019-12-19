f5775f4eef82552cbfd38a07239f0bca9060263c
stvnc <vincentangelo96@yahoo.com>
2019-12-19 12:28:31 +0700

    updated repo

--- daftar_profesi.py
+++ daftar_profesi.py
@@ -3,17 +3,19 @@ import pandas as pd
 
 df = pd.read_csv('profesi.csv', delimiter = '|')
 
-profesi= df['occupation'].unique()
-jumlahProfesi= df['occupation'].nunique()
+profesi = df['occupation'].unique()
+jumlahProfesi = df['occupation'].nunique()
 print (f'jumlah profesi : {jumlahProfesi}')
+print()
 
 newDf = df.groupby([df['occupation'],df['gender']]).describe()
 newDf = newDf['age'][['max','min','mean']]
-newDf.rename(columns={'max':'max_usia','min':'min_usia','mean':'rerata_usia'}, inplace=True)
+newDf.rename(columns = {'max': 'max_usia', 'min':'min_usia',' mean':'rerata_usia'}, inplace = True)
 print(newDf)
+print()
 
-gender = pd.crosstab(df.occupation, df.gender).apply(lambda r: r/r.sum(), axis=1) * 100
-gender['%total'] = gender['F']+gender['M']
-gender.rename(columns={'F':'%female','M':'%male'},inplace=True)
-gender = gender[['%male','%female','%total']]
+gender = pd.crosstab(df.occupation, df.gender).apply(lambda r: r/r.sum(), axis = 1) * 100
+gender['%total'] = gender['F'] + gender['M']
+gender.rename(columns = {'F': '%female', 'M': '%male'}, inplace = True)
+gender = gender[['%male', '%female', '%total']]
 print(gender)
