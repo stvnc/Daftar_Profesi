494fa8fe6d3169d6e2a3ea3178e655428114a14c
Vincent <vincentangelo96@yahoo.com>
2019-12-19 12:33:59 +0700

    Delete JCDS07SoalNo01Query02.sql

--- JCDS07SoalNo01Query02.sql
+++
@@ -1,7 +0,0 @@
-use sakila;
-
-select category.name as kategori, count(film.film_id) as jumlahMovie,
-avg(film.rental_rate) from category inner join film_category 
-on category.category_id = film_category.category_id 
-inner join film on film_category.film_id = film.film_id group by category.name 
-order by count(film.film_id) desc;
