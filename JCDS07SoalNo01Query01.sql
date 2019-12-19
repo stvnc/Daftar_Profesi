eb8656681a6c0222f23951e11370cccca7437401
Vincent <vincentangelo96@yahoo.com>
2019-12-19 12:33:52 +0700

    Delete JCDS07SoalNo01Query01.sql

--- JCDS07SoalNo01Query01.sql
+++
@@ -1,6 +0,0 @@
-use sakila;
-
-select film.title, category.name as category, film.length from film 
-inner join film_category on film.film_id = film_category.film_id
-inner join category on film_category.category_id = category.category_id
-where category.name = 'Comedy' order by film.length asc limit 10;
