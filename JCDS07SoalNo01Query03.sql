1546c9f57ac5007aac589edc3de54f64d029c1b2
Vincent <vincentangelo96@yahoo.com>
2019-12-19 12:34:07 +0700

    Delete JCDS07SoalNo01Query03.sql

--- JCDS07SoalNo01Query03.sql
+++
@@ -1,3 +0,0 @@
-select film.rating, any_value(ratingdesc.decription) as keterangan, count(film.film_id) from film
-inner join ratingdesc on film.rating = ratingdesc.rating_name
-group by film.rating order by film.rating asc;
