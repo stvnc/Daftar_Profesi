f9cbf073a502408828e53c84093beffc5905ff87
Vincent <vincentangelo96@yahoo.com>
2019-12-19 12:34:28 +0700

    Delete JCDS07SoalNo01Query07.sql

--- JCDS07SoalNo01Query07.sql
+++
@@ -1,8 +0,0 @@
-use sakila;
-
-select actor.actor_id, actor.first_name, actor.last_name, count(film.title) as jumlah_Movie
-from actor inner join film_actor on actor.actor_id = film_actor.actor_id
-inner join film on film_actor.film_id = film.film_id
-inner join film_category on film.film_id = film_category.film_id
-inner join category on film_category.category_id = category.category_id
-where category.name = 'Horror' group by actor.actor_id order by jumlah_Movie desc limit 10;
